d7929b95f1389a9dbbb10bd9c1a80960052c5486
Alisenaqubadi <131773898+Alisenaqubadi@users.noreply.github.com>
2025-09-27 16:11:27 +0500

    gameBoard and ship class were completed and finish

--- src/js/gameBoard.js
+++ src/js/gameBoard.js
@@ -1,5 +1,48 @@
-import { ship } from "./ship.js";
+import { Ship } from "./ship.js";
 
-class gameBoard{
-    
+class gameBoard {
+    constructor() {
+    this.board = Array(10).fill(null).map(() => Array(10).fill(null));
+    this.ships = [];
+    this.hits = [];
+    this.misses = [];
+  }
+
+  placeShip(ship, coordinates, orientation) {
+    const boardSize = 10; 
+
+    if (orientation === 'x' && x + ship.length > boardSize){
+    throw new Error("Ship goes out of bounds horizontally");}
+    if (orientation === 'y' && y + ship.length > boardSize){
+    throw new Error("Ship goes out of bounds vertically");}
+
+    for(let i = 0; i < ship.length; i++){
+        let x = coordinates[0] + ( orientation == "y" ? i : 0 )
+        let y = coordinates[1] + ( orientation == "x" ? i : 0 )
+        this.board[x][y] = ship;
+    }
+
+  }
+
+  receiveAttack(coordinates) {
+
+    const [ x , y ] = coordinates;
+    const BoardPiece = this.board[x][y]
+    if(BoardPiece === null ){
+        this.misses.push(coordinates)
+        return false
+    } else {
+        BoardPiece.hit();
+        this.hits.push(coordinates)
+        return true
+    }
+  }
+
+  allShipsSunk() {
+    return this.ships.every(ship => ship.isSunk());
+  }
+}
+
+export {
+    gameBoard,
 }
--- src/js/ship.js
+++ src/js/ship.js
@@ -1,121 +1,22 @@
-class ship {
-  Carrier = {
-    name: "Carrier",
-    length: 5,
-    head: false,
-    orientation: false,
-    squares: []
-  }
-  Battleship = {
-    name: "Battleship",
-    length: 4,
-    head: false,
-    orientation: false,
-    squares: []
-  }
-  Cruiser = {
-    name: "Cruiser",
-    length: 3,
-    head: false,
-    orientation: false,
-    squares: []
-  }
-  Submarine = {
-    name: "Submarine",
-    length: 3,
-    head: false,
-    orientation: false,
-    squares: []
-  }
-  Destroyer = {
-    name: "Destroyer",
-    length: 2,
-    head: false,
-    orientation: false,
-    squares: []
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
   }
 
-  BoardMisses = [];
-  BoardHits = [];
+  hit() {
+    this.hits++;
+    if (this.hits >= this.length) {
+      this.sunk = true;
+    }
+  }
 
-
-  place(shipName, where, orientation) {
-    
-  const ships = {
-    Carrier: this.Carrier,
-    Battleship: this.Battleship,
-    Cruiser: this.Cruiser,
-    Submarine: this.Submarine,
-    Destroyer: this.Destroyer
-  };
-
-  const ship = ships[shipName];
-  if (!ship) throw new Error("Unknown ship");
-
-  let [x, y] = where.map(Number);
-
-  const boardSize = 10; 
-  if (orientation === 'x' && x + ship.length > boardSize)
-    throw new Error("Ship goes out of bounds horizontally");
-  if (orientation === 'y' && y + ship.length > boardSize)
-    throw new Error("Ship goes out of bounds vertically");
-
-  ship.head = [x, y];
-  ship.orientation = orientation;
-  ship.squares = [];
-
-  for (let i = 0; i < ship.length; i++) {
-    if (orientation === 'x') ship.squares.push([x + i, y]);
-    else ship.squares.push([x, y + i]);
+  isSunk() {
+    return this.sunk;
   }
 }
 
-  hit( where ){
-    const check = this.checkHits(where);
-    if(check.hit) { 
-    this.BoardHits.push(check)
-    return check
-    }
-    else{ 
-      this.BoardMisses.push(where)
-      return check.hit
-    }
-  }
 
-  checkHits(where) {
-    const ships = [this.Carrier,this.Battleship,this.Submarine,this.Carrier,this.Destroyer]
-
-    for (let i = 0; i < ships.length; i++) {
-      if(ships[i].squares.map(String).includes(String(where))){
-        return {
-          ship: ships[i].name,
-          where: where,
-          hit: true
-        }
-      }
-    }
-
-    
-    return {hit: false}
-  }
-
-  isSunk(ship){
-    const ships = {
-    Carrier: this.Carrier,
-    Battleship: this.Battleship,
-    Cruiser: this.Cruiser,
-    Submarine: this.Submarine,
-    Destroyer: this.Destroyer
-  };
-
-    const currentShip = ships[ship].squares
-    const newHits = this.BoardHits.map(hit => hit.where);
-
-    return currentShip.every(square =>
-    newHits.some(hit => hit[0] === square[0] && hit[1] === square[1])
-  );
-  }
-
-}
-
-export { ship }
+export { Ship }
